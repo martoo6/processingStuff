6ea237e8741305b830dad509940dfe0fe658f567
martin <martin@Voyager.(none)>
2014-01-29 22:07:57 -0300

    Updated pixel trigonometry, great performance boost

--- PixelTrigonometry/Pixel_Trigonometry_4/Pixel_Trigonometry_4.pde
+++ PixelTrigonometry/Pixel_Trigonometry_4/Pixel_Trigonometry_4.pde
@@ -9,7 +9,7 @@ void setup(){
 void draw(){
   frame.setTitle(Integer.toString((int)(frameRate)));
   shader.set("time", (float) frameCount);
-  shader.set("detail", (int)map(mouseX, 0, width, 0, 255));
+  shader.set("detail", map(mouseX, 0, width, 0, 50)*pow(2,map(mouseY,0,height,0,10)));
   shader(shader);
   rect(0,0,width,height);
 }
--- PixelTrigonometry/Pixel_Trigonometry_4/data/mask.glsl
+++ PixelTrigonometry/Pixel_Trigonometry_4/data/mask.glsl
@@ -1,36 +1,23 @@
 #define PROCESSING_COLOR_SHADER
 
 uniform float time;
-uniform int detail;
+uniform float detail;
 
-void hsvToRgb(in float h,in float s,in float v){
-float r, g, b;
- 
-float i = h * 6;
-float f = h * 6 - i;
-float p = v * (1 - s);
-float q = v * (1 - f * s);
-float t = v * (1 - (1 - f) * s);
- 
-switch(mod(i,6)){
-    case 0: r = v; g = t; b = p; break;
-    case 1: r = q; g = v; b = p; break;
-    case 2: r = p; g = v; b = t; break;
-    case 3: r = p; g = q; b = v; break;
-    case 4: r = t; g = p; b = v; break;
-    case 5: r = v; g = p; b = q; break;
-}
- 
-gl_FragColor = vec4(r,g,b,1);
+//From: http://lolengine.net/blog/2013/07/27/rgb-to-hsv-in-glsl
+vec3 hsv2rgb(vec3 c)
+{
+    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
 }
         
 
 void main() {
-    //float val = 100.0 + (tan(gl_FragCoord.y*0.02) * cos(gl_FragCoord.x*0.2)*50 + tan(gl_FragCoord.x*0.02) * cos(gl_FragCoord.y*0.2) * 50) *cos(time * 0.01);
+    //float val = 100.0 + (tan(gl_FragCoord.y*0.02) * cos(gl_FragCoord.x*0.2)*50.0 + tan(gl_FragCoord.x*0.02) * cos(gl_FragCoord.y*0.2) * 50.0) *cos(time * 0.01);
     //float val = 150+tan(gl_FragCoord.x*gl_FragCoord.y*0.05)*cos(time*0.005);
-    precision highp float;
-    highp float val = (cos(gl_FragCoord.x*0.005)*cos(gl_FragCoord.y*0.005)-cos(time*0.002))*detail;
-  	hsvToRgb(val, 255.0, 255.0);
+    float val = (cos(gl_FragCoord.x*0.005)*cos(gl_FragCoord.y*0.005)-cos(time*0.02))*detail;
+  	vec3 res = hsv2rgb(vec3(val, 255.0, 255.0));
+  	gl_FragColor = vec4(res.r, res.g, res.b, 1.00);
 }
 
 
